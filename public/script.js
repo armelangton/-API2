3f3e34848f82597d94972370e5bf1f07f6556750
Amy Melangton <armelangton@gmail.com>
2021-05-19 18:10:34 -0400

    final edits

---
+++ public/script.js
@@ -0,0 +1,34 @@
+const key = 'PhFyTHUtLx1s8CajQCg7yWZctLsCrL1a';
+let searchBtn= document.querySelector("#search");
+let searchInput=document.getElementById("searchTerm");
+
+let div = document.querySelector(".results")
+
+function fetchResults() {
+  console.log("fetchResults called", searchInput.value);
+
+  var searchValue=searchInput.value;
+
+  const URL = `https://api.giphy.com/v1/gifs/search?api_key=${key}&limit=10&offset=0&rating=pg-13&lang=en&q=${searchValue}`;
+  console.log(URL);
+
+  fetch(URL)
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(data => display(data.data))
+  .catch(function(json) {
+  })
+}
+searchBtn.addEventListener("click", fetchResults);
+function display(data){
+  console.log(data)
+ data.forEach(item => {
+   
+   let image= document.createElement("img")
+   image.src=item.images.original.url
+   image.setAttribute("width","200px")
+   div.appendChild(image)
+  });
+}
